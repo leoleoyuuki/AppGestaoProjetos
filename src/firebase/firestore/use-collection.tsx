af11ce3a4bc9bbebf95c4a8fa7b37d85cb2aabd5
System Fabric <systemfabriccontato@gmail.com>
2026-02-22 01:47:39 +0000

    temo que o problema nao seja nas regras, mas se o auth.request chega par

--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -34,6 +34,7 @@ export interface InternalQuery extends Query<DocumentData> {
       canonicalString(): string;
       toString(): string;
     }
+    collectionGroup?: string;
   }
 }
 
@@ -85,11 +86,16 @@ export function useCollection<T = any>(
         setIsLoading(false);
       },
       (error: FirestoreError) => {
-        // This logic extracts the path from either a ref or a query
-        const path: string =
-          memoizedTargetRefOrQuery.type === 'collection'
-            ? (memoizedTargetRefOrQuery as CollectionReference).path
-            : (memoizedTargetRefOrQuery as unknown as InternalQuery)._query.path.canonicalString()
+        let path: string;
+        const internalQuery = memoizedTargetRefOrQuery as unknown as InternalQuery;
+
+        if (memoizedTargetRefOrQuery.type === 'collection') {
+          path = (memoizedTargetRefOrQuery as CollectionReference).path;
+        } else if (internalQuery._query.collectionGroup) {
+          path = `(collectionGroup: ${internalQuery._query.collectionGroup})`;
+        } else {
+          path = internalQuery._query.path.canonicalString();
+        }
 
         const contextualError = new FirestorePermissionError({
           operation: 'list',
