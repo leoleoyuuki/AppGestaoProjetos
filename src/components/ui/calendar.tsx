f707b8262e5537e81f4ee9bbae96cc8f831c7eb8
System Fabric <systemfabriccontato@gmail.com>
2026-02-24 14:42:59 +0000

    vamos tentar de outra forma: veja que o primeiro weekday e maior que tod

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -18,14 +18,23 @@ function Calendar({
 }: CalendarProps) {
   return (
     <DayPicker
+      locale={ptBR}
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
-      locale={ptBR}
+      formatters={{
+        formatWeekdayName: (date) =>
+          date
+            .toLocaleDateString("pt-BR", { weekday: "short" })
+            .replace(".", "")
+            .slice(0, 3),
+      }}
       classNames={{
         months: "flex flex-col space-y-4",
         month: "space-y-4",
+
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-sm font-medium",
+
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
@@ -33,26 +42,36 @@ function Calendar({
         ),
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
-        table: "w-full border-collapse space-y-1",
-        head_row: "flex",
+
+        table: "w-full border-collapse table-fixed",
+        head_row: "",
+        row: "",
+
         head_cell:
-          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem] text-center",
-        row: "flex w-full mt-2",
-        cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+          "text-muted-foreground font-normal text-[0.8rem] text-center w-9",
+        cell:
+          "h-9 w-9 text-center text-sm p-0 relative focus-within:z-20",
+
         day: cn(
           buttonVariants({ variant: "ghost" }),
           "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
         ),
-        day_range_end: "day-range-end",
+
         day_selected:
           "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
+
         day_today: "bg-accent text-accent-foreground",
+
         day_outside:
-          "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
+          "text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
+
         day_disabled: "text-muted-foreground opacity-50",
+
         day_range_middle:
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
+
         day_hidden: "invisible",
+
         ...classNames,
       }}
       components={{
@@ -63,6 +82,7 @@ function Calendar({
     />
   )
 }
+
 Calendar.displayName = "Calendar"
 
 export { Calendar }
